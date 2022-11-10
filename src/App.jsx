f89185bdda8f6ff07df9590c313aee7656b011df
allen <eepson123@gmail.com>
2022-11-11 00:15:09 +0800

    feat:Add the gsap init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef, useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const comp = useRef();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.to('.box', { rotation: '+=360' });
+      // ScrollTrigger.create({ trigger: '#my-id' });
+    }, comp);
+
+    return () => ctx.revert();
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="w-100 flex h-20 flex-1 bg-red-100">123123123123123123123</h1>
+      <div className="div" ref={comp}>
+        <h1 className="box  flex h-20 w-[200px] flex-1 bg-red-100">123123123123123123123</h1>
+      </div>
     </div>
   );
 }
