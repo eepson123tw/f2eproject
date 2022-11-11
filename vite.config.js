b8c0550f1ca21131b9a1eb519f000b58aabfbdca
allen <eepson123@gmail.com>
2022-11-12 00:03:23 +0800

    fix:fix the base vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/f2eproject/'
+  base: '/f2eproject/',
   plugins: [react()]
-})
+});
