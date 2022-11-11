c442b26dc96d3bfef5fb14178936ee2483e97edc
allen <eepson123@gmail.com>
2022-11-12 00:25:59 +0800

    feat:Add the deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:eepson123tw.github.io/f2eproject.git main:gh-pages
+
+cd -
