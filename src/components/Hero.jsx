2fe5a537d8528586f33b1daa15eee6a3f78c640a
allen <eepson123@gmail.com>
2022-11-16 00:28:48 +0800

    feat:Add the hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,78 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useRef, useLayoutEffect, useState, useEffect } from 'react';
 import background from '../assets/heroBig.svg';
+import backgroundSmall from '../assets/heroSmall.svg';
 import { gsap } from 'gsap';
 export default function Hero() {
   const app = useRef();
-  const tl = useRef();
+  const backGroundSmallStyle = { backgroundRepeat: 'no-repeat', backgroundPosition: '35% 100%' };
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
-      gsap.to('.box', {
-        x: 100,
-        repeat: -1,
-        repeatDelay: 1,
-        yoyo: true
+      const timeline = gsap.timeline();
+      const textTimeline = gsap.timeline();
+      gsap.to('.loader', {
+        delay: 1.3,
+        duration: 1,
+        scale: 6
+      });
+      textTimeline.to('.text_hero', {
+        opacity: 0,
+        alpha: 0,
+        delay: -1
+      });
+      textTimeline.to('.text_hero', {
+        opacity: 1,
+        alpha: 1,
+        delay: 3.5
+      });
+      timeline.to('.bg', {
+        backgroundSize: '120%',
+        backgroundPosition: '0 0',
+        delay: 1.3,
+        alpha: 0,
+        ease: 'linear',
+        duration: 1.5
+      });
+      timeline.to('.bg', {
+        backgroundImage: `url(${background})`,
+        ease: 'linear',
+        alpha: 1,
+        duration: 1
       });
     }, app);
-
     return () => ctx.revert();
   }, []);
 
+  useLayoutEffect(() => {
+    let text = document.querySelector('.text p');
+    text.innerHTML = text.innerText
+      .split('')
+      .map((char, i) => `<b style="transform:rotate(${i * 10}deg)">${char}</b>`)
+      .join('');
+  }, []);
+
+  // setTimeout(() => {
+  //   dos();
+  //   console.log(hasShow);
+  // }, 100);
+
   return (
-    <div
-      ref={app}
-      style={{ backgroundImage: `url(${background})` }}
-      className="relative flex h-[calc(100vh-96px)] items-center justify-center "
-    >
-      <div>
-        <h2 className="box absolute left-[257px] top-[301px] text-center text-white">
+    <div ref={app} className="relative flex h-[calc(100vh-96px)] items-center justify-center overflow-hidden">
+      <div
+        className="bg relative h-[100%] flex-1"
+        style={{ backgroundImage: `url(${backgroundSmall})`, ...backGroundSmallStyle }}
+      ></div>
+      <div className="loader">
+        <div className="text">
+          <p>TheF2E4thTheF2E4th</p>
+        </div>
+        <span>
+          <i></i>
+        </span>
+      </div>
+
+      <div className="text_hero">
+        <h2 className="absolute left-[257px] top-[301px] text-center text-white">
           <span className="flex text-[20vh]">
             The F<span className="mt-14 text-[15vh]">2</span>E
             <span className="relative mt-20 ml-10 block text-[15vh]">4</span>
