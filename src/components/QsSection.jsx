5f206de1914db0c2ff678257f1b6840c02696840
allen <eepson123@gmail.com>
2022-11-21 02:08:43 +0800

    feat:Add the slider animation

--- src/components/QsSection.jsx
+++ src/components/QsSection.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { gsap } from 'gsap';
 
 export default function QsSection() {
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      const a = gsap.to('.animation_ScrollBg', {
+        y: '50%',
+        duration: 10,
+        scrollTrigger: {
+          trigger: '.animation_ScrollArea',
+          start: 'top center',
+          end: 'bottom 20px',
+          // markers: true,
+          scrub: true
+        }
+      });
+    });
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <section className="before:contents-['']  relative flex h-[636px] before:absolute before:top-0 before:left-[50%] before:block before:h-[100%] before:w-[1px] before:bg-black">
+    <section className="before:contents-['']  animation_ScrollArea relative flex h-[636px] before:absolute before:top-0 before:left-[50%] before:block before:h-[100%] before:w-[1px] before:bg-black">
+      <div className="animation_ScrollBg"></div>
       <h2 className="seo_hidden">您是否有以下的困擾?</h2>
       <div className="flex flex-1 items-center justify-center">
-        <p className="text-[10vh] font-extrabold ">
+        <p className="animation_text text-[10vh] font-extrabold">
           您是否也有 <br />
           以下的困擾?
         </p>
       </div>
-      <div className="flex flex-1 items-center justify-center">
+      <div className="animation_text flex flex-1 items-center justify-center">
         <ul>
           <li className="mb-7 text-4xl font-extrabold">滿足不了同事的許願?</li>
           <li className="mb-7 text-4xl font-extrabold ">動畫技能樹太雜無從下手?</li>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Routes, Route, Outlet, Navigate, Link, useSearchParams } from 'react-router-dom';
 import FirstPage from './FirstPage';
 import SecondPage from './SecondPage';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 export default function App() {
+  gsap.registerPlugin(ScrollTrigger);
   return (
     <>
       <Routes>
